96fe7d28fb94283de8ff9682667b0e6cae2dc321
Vitor Marinheiro <vitorrodrigues@prefeitura.sp.gov.br>
2026-01-14 11:21:53 -0300

    feat: add cors

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -29,17 +29,21 @@ INSTALLED_APPS = [
     "rest_framework",
     "studyplanner",
     "drf_yasg",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True  # dev
 
 ROOT_URLCONF = 'setup.urls'
 
